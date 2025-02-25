d86093e5a5f43e58f4c155cb44855e46634abf30
Kofi Ayitey <e.kofiayitey@gmail.com>
2025-02-25 21:11:21 +0000

    still fixing

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// });
-
-// vite.config.js
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    historyApiFallback: true, // This ensures all routes fallback to index.html
-  },
+  plugins: [react()],
 });
